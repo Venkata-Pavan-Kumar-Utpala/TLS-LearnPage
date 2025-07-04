b1a25d9f98ef992dff88309498e34274f44d5af4
Ahmad Ullah <114727856+Ahmad264@users.noreply.github.com>
2025-07-04 21:59:41 +0530

    Build Page (hooks)

---
+++ frontend/src/hooks/useMidProjects.js
@@ -0,0 +1,20 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const useMidProjects = () => {
+  const [midProjects, setMidProjects] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    axios
+      .get(`${process.env.REACT_APP_API_URL}/mid-projects/build/mid-projects`)
+      .then((res) => setMidProjects(res.data))
+      .catch((err) => setError(err.message))
+      .finally(() => setLoading(false));
+  }, []);
+
+  return { midProjects, loading, error };
+};
+
+export default useMidProjects;
